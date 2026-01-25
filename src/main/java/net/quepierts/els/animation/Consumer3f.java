65199d74f54ba1ae3b360a6fe5c80d1e2b2b398a
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-01-25 23:53:25 +1000

    refractor(package): modid els -> endfield_panorama

--- src/main/java/net/quepierts/els/animation/Consumer3f.java
+++
@@ -1,6 +0,0 @@
-package net.quepierts.els.animation;
-
-@FunctionalInterface
-public interface Consumer3f {
-    void accept(float x, float y, float z);
-}
---
+++ src/main/java/net/quepierts/endfieldpanorama/animation/Consumer3f.java
@@ -0,0 +1,6 @@
+package net.quepierts.endfieldpanorama.animation;
+
+@FunctionalInterface
+public interface Consumer3f {
+    void accept(float x, float y, float z);
+}
